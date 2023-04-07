036faf5c4337b6971b73391e6bd053b4d75fdcac
gitgitWi <wiii@kakao.com>
2023-04-07 10:52:21 +0900

    feat: apply dummy-data component to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { DummyProducts } from '@/features/dummy-data';
+
 export default function HomePage() {
   return (
     <main className="flex flex-col w-full">
       <h1>Home</h1>
       <h2>Hello {process.env.hello}</h2>
+
+      <DummyProducts />
     </main>
   );
 }
